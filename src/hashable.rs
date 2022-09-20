456e202bbdc64c9d77051df19f9deda21f549194
Sataryus <69295311+Sataryus@users.noreply.github.com>
2022-09-20 17:27:14 +0300

    Learning blockchain

---
+++ src/hashable.rs
@@ -0,0 +1,8 @@
+use crate::Hash;
+pub trait Hashable {
+    fn bytes (&self) -> Vec<u8>;
+
+    fn hash (&self) -> Hash {
+        crypto_hash::digest(crypto_hash::Algorithm::SHA256, &self.bytes())
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,66 @@
+use blockchainlib::*;
+
+fn main () {
+    let difficulty = 0x000fffffffffffffffffffffffffffff;
+
+    let mut genesis_block = Block::new(0, now(), vec![0; 32], vec![
+        Transaction {
+            inputs: vec![ ],
+            outputs: vec![
+                transaction::Output {
+                    to_addr: "Alice".to_owned(),
+                    value: 50,
+                },
+                transaction::Output {
+                    to_addr: "Bob".to_owned(),
+                    value: 7,
+                },
+            ],
+        },
+    ], difficulty);
+
+    genesis_block.mine();
+
+    println!("Mined genesis block {:?}", &genesis_block);
+
+    let mut last_hash = genesis_block.hash.clone();
+
+    let mut blockchain = Blockchain::new();
+
+    blockchain.update_with_block(genesis_block).expect("Failed to add genesis block");
+
+    let mut block = Block::new(1, now(), last_hash, vec![
+        Transaction {
+            inputs: vec![ ],
+            outputs: vec![
+                transaction::Output {
+                    to_addr: "Chris".to_owned(),
+                    value: 536,
+                },
+            ],
+        },
+        Transaction {
+            inputs: vec![
+                blockchain.blocks[0].transactions[0].outputs[0].clone(),
+            ],
+            outputs: vec![
+                transaction::Output {
+                    to_addr: "Alice".to_owned(),
+                    value: 36,
+                },
+                transaction::Output {
+                    to_addr: "Bob".to_owned(),
+                    value: 12,
+                },
+            ],
+        },
+    ], difficulty);
+
+    block.mine();
+
+    println!("Mined block {:?}", &block);
+
+    last_hash = block.hash.clone();
+
+    blockchain.update_with_block(block).expect("Failed to add block");
+}
